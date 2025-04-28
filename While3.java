850507b3da251398b34285880fe8b7adfb588ceb
GabrielaGrandini <Gabriela.grandini1a@gmail.com>
2025-04-28 14:03:11 -0300

    Programa que soma os números pares de 1 a 100

---
+++ While3.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ Faça um programa que calcule e exiba a soma dos números pares de 1 a 100
+ utilizando um laço de repetição. Utilize while.
+*******************************************************************************/
+
+import java.util.Scanner;
+public class While3
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner (System.in);
+	    int soma = 0, n = 1;
+	    
+	    while (n <= 100){
+	        n++;
+	        if (n%2 == 0){
+	        soma+= n;
+	        }
+	    }
+		System.out.println("A soma doa números pares de 1 a 100 é: " + soma);
+	}
+}
